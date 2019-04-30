9f0555f82347cd2d099bbcc0e069e3e81ddbdf2f
Syeda Sana Zehra <sana_sz91@hotmail.com>
2019-05-01 00:19:27 +0500

    added some initial code

---
+++ lib/Payment/screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class PaymentPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => PaymentPageState();
+}
+
+class PaymentPageState extends State<PaymentPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Payment'),
+      ),
+      body: Container(),
+    );
+  }
+}
